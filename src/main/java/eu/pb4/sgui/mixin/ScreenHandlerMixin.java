39e9eccd5a32d3ce318fd107e9544210a1f4f2bb
IAmSneak <csudduth04@gmail.com>
2023-02-17 03:28:30 -0500

    fixed the issue (the issue was me)

--- src/main/java/eu/pb4/sgui/mixin/ScreenHandlerMixin.java
+++ src/main/java/eu/pb4/sgui/mixin/ScreenHandlerMixin.java
@@ -1,6 +1,7 @@
 package eu.pb4.sgui.mixin;
 
 import eu.pb4.sgui.virtual.inventory.VirtualInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,10 +10,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ScreenHandler.class)
-public abstract class ScreenHandlerMixin {
+public class ScreenHandlerMixin {
 
-    @Inject(method = "canInsertIntoSlot(Lnet/minecraft/screen/slot/Slot;)Z", at = @At("HEAD"), cancellable = true)
-    private static void blockIfVirtual(Slot slot, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "canInsertItemIntoSlot", at = @At("HEAD"), cancellable = true)
+    private static void blockIfVirtual(Slot slot, ItemStack stack, boolean allowOverflow, CallbackInfoReturnable<Boolean> cir) {
         if (slot.inventory instanceof VirtualInventory) {
             cir.setReturnValue(false);
         }
