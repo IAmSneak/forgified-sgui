a87418fff24bc1063d17275e7e60fc4e2ddd2cd8
IAmSneak <csudduth04@gmail.com>
2023-02-17 01:09:57 -0500

    update java level

--- src/main/java/eu/pb4/sgui/mixin/ScreenHandlerMixin.java
+++ src/main/java/eu/pb4/sgui/mixin/ScreenHandlerMixin.java
@@ -9,13 +9,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ScreenHandler.class)
-public abstract class ScreenHandlerMixin {
+public class ScreenHandlerMixin {
 
     @Inject(method = "canInsertIntoSlot(Lnet/minecraft/screen/slot/Slot;)Z", at = @At("HEAD"), cancellable = true)
     private static void blockIfVirtual(Slot slot, CallbackInfoReturnable<Boolean> cir) {
-        //if (slot.inventory instanceof VirtualInventory) {
-        //    cir.setReturnValue(false);
-        //}
-        cir.setReturnValue(false);
+        if (slot.inventory instanceof VirtualInventory) {
+            cir.setReturnValue(false);
+        }
     }
 }
